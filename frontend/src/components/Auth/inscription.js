cd61262f0922b8715137d21a6b45305e25f21ffe
Ulysse Dahiez <dahiez.ulysse@gmail.com>
2023-04-14 08:50:35 +0200

    offer front ok, login dont work

--- frontend/src/components/Auth/inscription.js
+++ frontend/src/components/Auth/inscription.js
@@ -17,68 +17,96 @@ export default class inscription extends React.Component {
         confirmPassWord:null
     }
     
-    handleSubmit(event) {}
+    handleSubmit(event) {
+        event.preventDefault();
+    
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: this.emailInput.current.value,
+                password: this.passwordInput.current.value,
+                firstName: this.firstNameInput.current.value,
+                lastName: this.lastNameInput.current.value
+            })
+        };
+    
+        fetch('http://localhost:3000/auth/register', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.emailInput && data.passwordInput && data.firstNameInput && data.lastNameInput) {
+                this.setState({
+                    lastName: data.lastNameInput,
+                    firstName: data.firstNameInput,
+                    email: data.emailInput,
+                    password: data.passwordInput,
+                    confirmPassWord: data.confirmPassWordInput
+                });
+                this.props.navigate('profile');
+            }
+        });
+    }
 
 
     render() {
 		return (
             <div className='layoutForm'>
-            <div className="container">
-            <div className="titleForm">
-                <a>Inscription</a>
+                <div className="container">
+                    <div className="titleForm">
+                        <a>Inscription</a>
+                    </div>
+                    <form className="inscriptionForm" onSubmit={ this.handleSubmit}>
+                        <div className="row">
+                            <div className="col-25">
+                                <label htmlFor="lastName">Nom* </label>
+                            </div> 
+                            <div className="col-75">
+                                <input required type="text" id="lastName" ref={this.lastNameInput} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-25">
+                                <label htmlFor="firstName">Prénom* </label>
+                            </div>
+                            <div className="col-75">
+                                <input required type="text" id="firstName" ref={this.firstNameInput} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-25">
+                                <label htmlFor="email">Email* </label>
+                            </div>
+                            <div className="col-75">
+                                <input required type="email" id="email" ref={this.emailInput} />
+                            </div>
+                        </div>
+                        <div className="row">   
+                            <div className="col-25">
+                                <label htmlFor="passWord">Mot de passe* </label>
+                            </div>
+                            <div className="col-75">
+                                <input required type="password" id="passWord" ref={this.passwordInput} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-25">
+                                <label htmlFor="confirmPassword"> Confirmation mot de passe* </label>
+                            </div>
+                            <div className="col-75">
+                                <input required type="password" id="ConfirmpassWord" ref={this.confirmPassWordInput} />
+                            </div>
+                        </div>
+                        
+                        <div className="row">
+                            <button type="submit">Inscription</button>
+                        </div>
+                    </form>
+                    <div className="row">
+                            <div className="connexion">
+                                <NavLink to="/connexion">Connexion</NavLink>
+                            </div>
+                    </div>
                 </div>
-                <form className="inscriptionForm" onSubmit={event => this.handleSubmit(event)}>
-                    <div className="row">
-                        <div className="col-25">
-                            <label for="lastName">Nom* </label>
-                        </div> 
-                        <div className="col-75">
-                            <input required type="text" id="lastName" ref={this.LastName} />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-25">
-                            <label for="firstName">Prénom* </label>
-                        </div>
-                        <div className="col-75">
-                            <input required type="text" id="firstName" ref={this.firstName} />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-25">
-                            <label for="email">Email* </label>
-                        </div>
-                        <div className="col-75">
-                            <input required type="email" id="email" ref={this.email} />
-                        </div>
-                    </div>
-                    <div className="row">   
-                        <div className="col-25">
-                            <label for="passWord">Mot de passe* </label>
-                        </div>
-                        <div className="col-75">
-                            <input required type="password" id="passWord" ref={this.password} />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-25">
-                            <label for="confirmPassword"> Confirmation mot de passe* </label>
-                        </div>
-                        <div className="col-75">
-                            <input required type="password" id="ConfirmpassWord" ref={this.confirmPassWord} />
-                        </div>
-                    </div>
-                    
-                    <div className="row">
-                        <button type="submit">Inscription</button>
-                    </div>
-                </form>
-                <div className="row">
-                        <div className="connexion">
-                            <NavLink to="/connexion">Connexion</NavLink>
-                        </div>
-                </div>
-            </div>
             </div>
         )
     }
--- frontend/src/components/offers.js
+++ frontend/src/components/offers.js
@@ -1,5 +1,40 @@
+import React, { Component } from 'react';
+import offers from '../../data/components/offers.json'
+import '../../css/offers.css'
+export default class Offers extends React.Component {
 
-export default function Offers() {
+   
 
-    return(<h1>mes offres</h1>);
+    render() {
+		return (
+            <div className="container">
+                <div className="titleForm">
+                    <a>Nos Offres</a>
+                </div>
+                <div className='layoutOffers'>
+
+                        {offers.data.map(offer => (
+                                <div className='offerContainer' key={offer.id}>
+                                    <div className="cardTitleOffer">{offer.name}</div>
+                                        <div className="cardImgsOffer"> 
+                                            <img className="cardImgOffer" src={`../../img/offers/${offer.photo[0]}.webp`} />
+                                            <img className="cardImgOffer" src={`../../img/offers/${offer.photo[1]}.webp`} />
+                                        </div>
+                                    <p className="card_text">{offer.description}</p>
+                                    <div className='row'>
+                                        <div className="col-1">
+                                            <div className="priceOffer">{offer.price} €</div>
+                                        </div>
+                                        <div className="col-2">
+                                            <button className="buyOffer">J'achète</button>
+                                        </div>
+                                    </div>
+                                    
+                            </div>
+                        ))}
+                    
+                </div>
+            </div>
+        )
+    }
 }
