ef2de9cb2ce610196727acf34eee7f807098ad53
Ulysse Dahiez <dahiez.ulysse@gmail.com>
2023-04-26 13:40:21 +0200

    maison administration fonctionne, css pas fini

--- frontend/src/services/sensor.service.js
+++ frontend/src/services/sensor.service.js
@@ -4,6 +4,16 @@ let getSensors = id => {
 	return Axios.get(`areas/${id}/sensors`);
 };
 
+let createSensor = (credentials, areaId) => {
+	return Axios.post(`/areas/${areaId}/sensors`, credentials);
+}
+
+let deleteSensor = (id) => {
+	return Axios.delete(`/sensors/${id}`);
+}
+
 export const sensorService = {
 	getSensors,
+	createSensor, 
+	deleteSensor
 };
